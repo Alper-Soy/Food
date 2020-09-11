e20384c9e34e5b200837a5c9c78538665527327b
Alper Soy <51034169+Alper-Soy@users.noreply.github.com>
2020-09-11 21:38:38 +0300

    Extracting Hook Logic

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,31 +1,11 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
-import yelp from '../api/yelp';
+import useResults from '../hooks/useResults';
 
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
-  const [results, setResults] = useState([]);
-  const [errorMessage, setErrorMessage] = useState('');
-
-  const searchApi = async (searchTerm) => {
-    console.log("hi there!")
-    try {
-      const response = await yelp.get('/search', {
-        params: {
-          limit: 50,
-          term: searchTerm,
-          location: 'san jose',
-        },
-      });
-      setResults(response.data.businesses);
-    } catch (err) {
-      setErrorMessage('Something went wrong!');
-    }
-  };
-
-  //Call searchApi when component is first rendered.
-  //searchApi('pasta'); => Infinite loop!!!...
+  const [results, errorMessage, searchApi] = useResults();
 
   return (
     <View>
