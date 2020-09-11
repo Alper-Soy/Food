67754abce3551727e71d943397877fd742605a0e
Alper Soy <51034169+Alper-Soy@users.noreply.github.com>
2020-09-11 17:53:23 +0300

    Detecting Editing Completion

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,11 +2,19 @@ import React from 'react';
 import { View, StyleSheet, TextInput } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 
-const SearchBar = () => {
+const SearchBar = ({ term, onTermChange, onTermSubmitting }) => {
   return (
     <View style={styles.backgroundStyle}>
       <Feather name='search' style={styles.iconStyle} />
-      <TextInput style={styles.inputStyle} placeholder='Search' />
+      <TextInput
+        autoCapitalize='none'
+        autoCorrect={false}
+        style={styles.inputStyle}
+        placeholder='Search'
+        value={term}
+        onChangeText={onTermChange}
+        onEndEditing={onTermSubmitting}
+      />
     </View>
   );
 };
